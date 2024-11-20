bfe5740804e045f3eeb18dce0c00e74fb76fd26f
WinTux <rusok.software@gmail.com>
2024-11-19 20:38:12 -0400

    Funcionalidad completa con DDBB

--- kafkaconsumer/src/main/java/com/wintux/consum/KafkaDDBBconsumer.java
+++ kafkaconsumer/src/main/java/com/wintux/consum/KafkaDDBBconsumer.java
@@ -1,5 +1,7 @@
 package com.wintux.consum;
 
+import com.wintux.consum.Models.RegistroWikimedia;
+import com.wintux.consum.Repositorios.WikimediaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -8,8 +10,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaDDBBconsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDDBBconsumer.class);
+    private WikimediaRepository datosWikiRepo;
+    public KafkaDDBBconsumer(WikimediaRepository datosWikiRepo){
+        this.datosWikiRepo = datosWikiRepo;
+    }
     @KafkaListener(topics = "cambios_de_wikimedia",groupId = "miGrupo")
     public void consumir(String eventMessage){
         LOGGER.info(String.format("Mensaje recibido en KafkaDDBBconsumer: %s", eventMessage));
+        RegistroWikimedia registro = new RegistroWikimedia();
+        registro.setWikiDatosDeEvento(eventMessage);
+        datosWikiRepo.save(registro);
     }
 }
---
+++ kafkaconsumer/src/main/java/com/wintux/consum/Repositorios/WikimediaRepository.java
@@ -0,0 +1,8 @@
+package com.wintux.consum.Repositorios;
+
+import com.wintux.consum.Models.RegistroWikimedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaRepository extends JpaRepository<RegistroWikimedia,Long> {
+
+}
